5a2a65c6792a4a653e375074b15b3f4834580def
vionescu <vionescu@amberstudio.ro>
2018-10-24 12:03:37 +0300

    Entity class with tests

--- server_config/entity.py
+++ server_config/entity.py
@@ -3,29 +3,34 @@
 
 class Entity(dict):
     """
-    An entity object. Can be accessed as a dict or as an obj. The attributes of
-    the entity will be created dynamically. For example, the following are both
-    valid::
-        entity = Entity()
-        entity.a = 'b'
-        entity['x'] = 'y'
-
-        entity = Entity(**json.loads(JSON_DATA))
+    Class to create attributes for dictionary fields
+    Inspired by https://stackoverflow.com/a/3031270/1722325
+    Added list capability in __setitem__
+    Supports creating Entitys recursively
     """
-    def __getattr__(self, name):
-        try:
-            return self[name]
-        except KeyError:
-            raise AttributeError("Attribute {} for object of type {} is missing".format('Entity', name))
+    MARKER = object()
 
-    __setattr__ = dict.__setitem__
+    def __init__(self, value=None):
+        if value is None:
+            pass
+        elif isinstance(value, dict):
+            for key in value:
+                self.__setitem__(key, value[key])        
+        else:
+            raise TypeError('expected dict')
 
-    def __delattr__(self, name):
-        try:
-            del self[name]
-        except KeyError:
-            raise AttributeError("Attribute {} for object of type {} is missing".format('Entity', name))
+    def __setitem__(self, key, value):
+        if isinstance(value, dict) and not isinstance(value, Entity):
+            value = Entity(value)
+        elif isinstance(value, list):
+            value = [Entity(o) if (isinstance(o, dict)) else o for o in value]
+        super(Entity, self).__setitem__(key, value)
 
-    def __dir__(self):
-        return dir({}) + list(self.keys())
+    def __getitem__(self, key):
+        found = self.get(key, Entity.MARKER)
+        if found is Entity.MARKER:
+            found = Entity()
+            super(Entity, self).__setitem__(key, found)
+        return found
 
+    __setattr__, __getattr__ = __setitem__, __getitem__
--- tests/test_entity.py
+++ tests/test_entity.py
@@ -1,13 +1,31 @@
 from unittest import TestCase
+from nose.tools import raises
 from server_config.entity import Entity
 
+
 class TestEntity(TestCase):
+
     def test_simple_dict(self):
-        test_object = {"a": 3 }
-        e = Entity(test_object)        
+        data = {"a": 3}
+        e = Entity(data)
         self.assertTrue(e.a == 3)
 
-    def test_complex_dict(self):
-        test_object = {"a": { "b": 3} }
-        e = Entity(test_object)        
+    def test_list(self):
+        data = {"list": [1, 2, 3, 4]}
+        e = Entity(data)
+        self.assertTrue(e.list == [1, 2, 3, 4])
+
+    def test_list_of_dict(self):
+        data = {"list": [{"e1": 1}, {"e2": 2}]}
+        e = Entity(data)
+        self.assertTrue(e.list[0].e1 == 1)
+
+    def test_dict_in_dict(self):
+        test_object = {"a": {"b": 3}}
+        e = Entity(test_object)
         self.assertTrue(e.a.b == 3)
+
+    @raises(TypeError)
+    def test_wrong_type(self):
+        test_object = [1, 2]
+        Entity(test_object)        
